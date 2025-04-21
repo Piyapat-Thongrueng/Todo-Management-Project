6054a35940c017e37a18c14a32ffb9faf8bdfcc6
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-21 09:51:35 +0700

    Install and configure Model Mapper

--- src/main/java/com/piyapatproject/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/piyapatproject/todo/service/impl/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import com.piyapatproject.todo.entity.Todo;
 import com.piyapatproject.todo.repository.TodoRepository;
 import com.piyapatproject.todo.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,23 +13,19 @@ import org.springframework.stereotype.Service;
 public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
         // Convert TodoDto into TodoJpa entity
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.isCompleted());
+        Todo todo = modelMapper.map(todoDto, Todo.class);
+
+        // save this jpa entity to database.
         Todo savedTodo = todoRepository.save(todo);
 
         // Convert saved TodoJpa entity object into TodoDto object
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
 
         return savedTodoDto;
     }
